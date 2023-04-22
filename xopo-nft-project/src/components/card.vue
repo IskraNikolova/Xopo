1599295e6e89860af8552f42f3ce5d2f967e487f
IskraNikolova <air_iskra@yahoo.com>
2023-04-22 09:40:20 +0300

    Title: Updated Card Sizes and Added Custom "Card" Component
    Description:
    In this commit, I have made some improvements to the card sizes on the dApp. I have fixed the card sizes. Additionally, I have added a new custom "card" component that can be used throughout the dApp.
    
    The new "card" component provides more flexibility in designing the dApp's layout. It allows for customization of the card's appearance, including colors, fonts, and spacing. This will make it easier to create visually appealing pages and improve the overall user experience.
    
    Overall, this commit improves the dApp's design and functionality. The updated card sizes and new "card" component will make it easier to create attractive and consistent pages throughout the dApp.

---
+++ xopo-nft-project/src/components/card.vue
@@ -0,0 +1,38 @@
+<template>
+   <table class="q-mr-xs" style="width: 330px; height: 350px; background-color: black;border-color: black; position: relative;">
+        <tr>
+            <td style="text-align: center; vertical-align: middle;">
+                <img
+                :id="item.name"
+                :src="item.url"
+                style="max-width: 250px;max-height: 200px"
+                >
+            </td>
+            </tr>
+            <tr>
+            <td colspan="2" style="text-align: center;">
+                <div style="z-index:10; position: absolute; top: 0; left: 0; width: 100%; height: 20%; background-color: rgba(0, 0, 0, 0.3);">
+                <p style="color: white; font-size: 20px; margin-top: 40px; margin-left: 120px;z-index:2;">{{ item.name }}</p>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align: left;">
+                <p style="color: grey; font-size: 16px;margin-left: 20px;">by Miroslav Mihov</p>
+                <p style="color: white; font-size: 18px;margin-left: 20px;">{{ item.description }}</p>
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- xopo-nft-project/src/pages/Dashboard.vue
+++ xopo-nft-project/src/pages/Dashboard.vue
@@ -1,37 +1,17 @@
 <template>
-    <q-page>
-      <div class="q-mt-xl q-ml-xl q-mr-md q-pa-md row items-start q-gutter-md">
-        <div v-for=" (item, i) in all" v-bind:key="i">
-          <table class="q-mr-xs" style="width: 300px; height: 350px; background-color: black;border-color: black; position: relative;">
-          <tr>
-            <td style="text-align: center; vertical-align: middle;">
-              <img
-                :id="item.name"
-                :src="item.url"
-                style="max-width: 300px;max-height: 200px"
-                @mouseover="onOver(item.name)"
-                @mouseleave="onLeave(item.name)"
-              >
-            </td>
-          </tr>
-          <tr>
-            <td colspan="2" style="text-align: center;">
-              <div style="z-index:10; position: absolute; top: 0; left: 0; width: 100%; height: 20%; background-color: rgba(0, 0, 0, 0.3);">
-                <p style="color: white; font-size: 20px; margin-top: 40px; margin-left: 120px;z-index:2;">{{ item.name }}</p>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="2" style="text-align: left;">
-              <p style="color: grey; font-size: 16px;margin-left: 20px;">by Miroslav Mihov</p>
-              <p style="color: white; font-size: 18px;margin-left: 20px;">{{ item.description }}</p>
-            </td>
-          </tr>
-        </table>
-        </div>
+  <q-page>
+    <div
+      class="q-mt-md row q-gutter-md"
+      style="display: flex;
+      justify-content: center;
+      flex-wrap: wrap"
+    >
+      <div v-for=" (item, i) in all" v-bind:key="i">
+        <card v-bind:item="item"/>
       </div>
-    </q-page>
-  </template>
+    </div>
+  </q-page>
+</template>
 
 <script>
 import {
@@ -47,6 +27,9 @@ import {
 
 export default {
   name: 'PageDashboard',
+  components: {
+    Card: () => import('components/card.vue')
+  },
   data () {
     return {
       ids: [],
@@ -69,21 +52,13 @@ export default {
       'appTheme'
     ])
   },
-  async created () {
+  async created () { // todo make on store
     await this.getIds()
     await this.getAll()
   },
   methods: {
     async getIds () {
       this.ids = await _getNFTByAddress(this.userAddress)
-    },
-    // todo
-    onOver (id) {
-      document.getElementById(id).style.maxWidth = '350px'
-    },
-    // todo
-    onLeave (id) {
-      document.getElementById(id).style.maxWidth = '300px'
     },
     async getData (id) {
       try {
@@ -111,11 +86,4 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 250px
-.my-card-section
-  height: 100%
-  max-height: 210px
-  min-height: 120px
 </style>
