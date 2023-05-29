e75cea1ce8fa8004f3dbada120513c7af8837aa5
IskraNikolova <air_iskra@yahoo.com>
2023-05-29 15:37:05 +0300

    Optimized the design to adapt smoothly across different screen sizes

--- xopo-nft-project/src/pages/Index.vue
+++ xopo-nft-project/src/pages/Index.vue
@@ -11,12 +11,12 @@
       <p class="text-uppercase text-bold text3" style="font-size: 18pt;">Decentralized hub for artists, developers, and free speech enthusiasts.</p>
       <q-img src="~assets/icons/mouse.svg" style="width: 2%;margin-top: 8%;"/>
     </div>-->
-    <div class="text-center text2">
-      <p class="text-regular" style="font-size: 13pt; margin-bottom: 1%;">We're crafting a worldwide platform for creatives of all kinds, with</p>
-      <div class="text-montserrat-bold" style="font-size: 42pt; margin-bottom: 1%;">
+    <div class="text2 absolute-top absolute-center">
+      <p class="text-regular text-center" style="font-size: 13pt; margin-bottom: 1%;">We're crafting a worldwide platform for creatives of all kinds, with</p>
+      <div class="text-montserrat-bold text-center" style="font-size: 42pt; margin-bottom: 1%;">
         <span style="background-color: #1f1f1e; padding: 5px; color: white;">NFTs</span> as <img src="~assets/logos/three_logo.svg" style="width: 13%; vertical-align: middle; margin-bottom: 7px;" />ur Canvas
       </div>
-      <p class="text-uppercase text-bold text3" style="font-size: 18pt;">Decentralized hub for artists, developers, and free speech enthusiasts.</p>
+      <p class="text-uppercase text-bold text3 text-center" style="font-size: 18pt;">Decentralized hub for artists, developers, and free speech enthusiasts.</p>
       <div class="text-center q-mb-xl">
         <span class="q-mr-xs"><q-img src="~assets/icons/media/discord_i.svg" style="width: 20pt;" /></span>
         <span class="q-mr-xs"><q-img src="~assets/icons/media/twiter_i.svg" style="width: 20pt;" /></span>
@@ -24,7 +24,7 @@
         <span class="q-mr-xs"><q-img src="~assets/icons/media/telegram_i.svg" style="width: 20pt;" /></span>
         <span class="q-mr-xs"><q-img src="~assets/icons/media/medium_i.svg" style="width: 20pt;" /></span>
       </div>
-      <div style="position: relative;min-width: 180%;margin-top: 12%;margin-left: -10%;">
+      <div style="position: relative;min-width: 180%;margin-top: 1%;margin-left: 70%;">
         <span class="anime"></span>
         <span class="anime2"></span>
         <span class="anime3"></span>
@@ -91,43 +91,43 @@ export default {
     window.removeEventListener('scroll', this.handleScroll)
   },
   methods: {
-    // handleScroll () {
-    //   const h1 = document.querySelector('.h1')
-    //   const logo = document.querySelector('.logo')
-    //   const text = document.querySelector('.text')
-    //   const tText = document.querySelector('.third-text')
-    //   const fText = document.querySelector('.first-text')
-    //   const sText = document.querySelector('.second-text')
-    //   const scrollY = window.scrollY
-    //   const position = Math.min(10 + scrollY * 0.2, window.innerHeight)
+    handleScroll () {
+      // const h1 = document.querySelector('.h1')
+      // const logo = document.querySelector('.logo')
+      // const text = document.querySelector('.text')
+      // const tText = document.querySelector('.third-text')
+      // const fText = document.querySelector('.first-text')
+      // const sText = document.querySelector('.second-text')
+      // const scrollY = window.scrollY
+      // const position = Math.min(10 + scrollY * 0.2, window.innerHeight)
 
-    //   if (scrollY >= 570) {
-    //     tText.classList.add('show4')
-    //   } else if (scrollY > 520) {
-    //     sText.classList.add('show3')
-    //   } else if (scrollY > position) {
-    //     fText.classList.add('show2')
-    //   } else if (scrollY > 0) {
-    //     text.classList.add('show')
-    //     logo.classList.add('logo-small', 'logo-below')
-    //   }
+      // if (scrollY >= 570) {
+      //   tText.classList.add('show4')
+      // } else if (scrollY > 520) {
+      //   sText.classList.add('show3')
+      // } else if (scrollY > position) {
+      //   fText.classList.add('show2')
+      // } else if (scrollY > 0) {
+      //   text.classList.add('show')
+      //   logo.classList.add('logo-small', 'logo-below')
+      // }
 
-    //   if (scrollY <= 0) {
-    //     h1.innerHTML = 'NFTs as our canvas'
-    //     text.classList.remove('show')
-    //     logo.classList.remove('logo-small', 'logo-below')
-    //   } else if (scrollY <= position) {
-    //     h1.innerHTML = '<span>NFTs as </span><span>&nbsp; &nbsp;<span>ur canvas</span>'
-    //     fText.classList.remove('show2')
-    //   } else if (scrollY <= 520) {
-    //     sText.classList.remove('show3')
-    //   } else if (scrollY <= 600) {
-    //     tText.classList.remove('show4')
-    //   }
+      // if (scrollY <= 0) {
+      //   h1.innerHTML = 'NFTs as our canvas'
+      //   text.classList.remove('show')
+      //   logo.classList.remove('logo-small', 'logo-below')
+      // } else if (scrollY <= position) {
+      //   h1.innerHTML = '<span>NFTs as </span><span>&nbsp; &nbsp;<span>ur canvas</span>'
+      //   fText.classList.remove('show2')
+      // } else if (scrollY <= 520) {
+      //   sText.classList.remove('show3')
+      // } else if (scrollY <= 600) {
+      //   tText.classList.remove('show4')
+      // }
 
-    //   logo.style.top = `${position}px`
-    //   logo.style.opacity = `${Math.max(1 - scrollY / window.innerHeight, 0)}`
-    // }
+      // logo.style.top = `${position}px`
+      // logo.style.opacity = `${Math.max(1 - scrollY / window.innerHeight, 0)}`
+    }
   }
 }
 </script>
@@ -145,19 +145,10 @@ export default {
 
 .text {
   position: relative;
-  top: 45%;
   transform-style: preserve-3d;
   transition: transform 0.6s;
 }
 
-.text2 {
-  position: absolute;
-  top: 10%;
-  left: 29%;
-  transform-style: preserve-3d;
-  transition: transform 0.6s;
-  z-index: 2;
-}
 .text3 {
   max-width: 600px;
   margin: 0 auto;
@@ -233,58 +224,59 @@ export default {
   opacity: 1;
   transform: translateY(0);
 }
+
 .anime {
   position: absolute;
-  top: -100px;
-  left: 0px;
+  top: -8%;
+  left: 0%;
   display: block;
-  width: 220px;
-  height: 220px;
+  width: 18%;
+  height: 150pt;
   background-image: url('./../assets/animeImages/65.png');
   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
   animation: organic 12s infinite alternate ease-in-out;
 }
 .anime2 {
   position: absolute;
-  top: 0px;
-  left: 150px;
+  top: 0%;
+  left: 10%;
   display: block;
-  width: 120px;
-  height: 120px;
-  background-color: black;
+  width: 10%;
+  height: 100pt;
+  background-color: #1f1f1e;
   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
   animation: organic 12s infinite alternate ease-in-out;
 }
 .anime3 {
   position: absolute;
-  top: -25px;
-  left: 280px;
+  top: -1%;
+  left: 20%;
   display: block;
-  width: 200px;
-  height: 200px;
+  width: 16%;
+  height: 160pt;
   background-image: url('./../assets/animeImages/1.png');
   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
   animation: organic 12s infinite alternate ease-in-out;
 }
 .anime4 {
   position: absolute;
-  top: -90px;
-  left: 480px;
+  top: -5%;
+  left: 36%;
   display: block;
-  width: 240px;
-  height: 240px;
+  width: 20%;
+  height: 200pt;
   background-image: url('./../assets/animeImages/2.png');
   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
   animation: organic 12s infinite alternate ease-in-out;
 }
 .anime5 {
   position: absolute;
-  top: -140px;
-  left: 650px;
+  top: -5%;
+  left: 48%;
   display: block;
-  width: 130px;
-  height: 130px;
-  background-color: black;
+  width: 10%;
+  height: 100pt;
+  background-color: #1f1f1e;
   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
   animation: organic 12s infinite alternate ease-in-out;
 }
@@ -306,4 +298,12 @@ export default {
     border-radius: 70% 30% 30% 70% / 70% 70% 30% 30%;
   }
 }
+  .text2 {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: flex-start;
+   height: 100%;
+   margin-top: 5%;
+ }
 </style>
